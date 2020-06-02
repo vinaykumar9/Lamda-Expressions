367dafcd0ed548c089be35c6b70d91b0c24503a3
vinaykumar34 <gvinaykumar434@gmail.com>
2020-06-02 15:30:23 +0530

    the data files added to the browser

---
+++ src/com/lamda/workspace/LamdaDemo.java
@@ -0,0 +1,22 @@
+package com.lamda.workspace;
+
+public class LamdaDemo {
+
+	public static void main(String[] args) {
+WebPage w1 = (String value) -> {System.out.println("data"+value);};
+	
+	
+
+
+WebPage w2 = (int a,int b)-> {
+	System.out.println("raja"+ a+b);
+};
+
+
+w2.header("subbu");
+
+   w1.header("ramu");
+	}
+	
+
+}
---
+++ src/com/lamda/workspace/WebPage.java
@@ -0,0 +1,11 @@
+package com.lamda.workspace;
+
+public interface WebPage {
+	
+	//void header(String value);
+	void add(int a,int b);
+	//void header(String a, String b);
+//	void header(int value);
+
+
+}
